f319fd37ea87492863b84626b280a97cc4a6fd3b
Scott Stafford <scott.stafford@databricks.com>
2020-06-25 15:46:57 -0400

    Rewrite stack.py

--- stack.py
+++ stack.py
@@ -1,17 +1,34 @@
 import configparser
 import json
 
-config = configparser.ConfigParser()
-config.read('stack.properties')
-workspace_home = config["DATABRICKS"]["WORKSPACE_HOME"]
 
-with open('./config.json') as f:
-    config_json = json.load(f)
+def getProperties(propertiesName):
+    properties = configparser.ConfigParser()
+    properties.read(propertiesName)
+    return properties
 
-for resource in config_json.get("resources"):
-    path = resource.get("properties").get("path")
-    new_workspace_path = path.replace("%WORKSPACE_HOME%", workspace_home)
-    resource["properties"]["path"] = new_workspace_path
 
-with open('config-new.json', 'w') as new_config:
-    json.dump(config_json, new_config, indent=4)
+def getConfig(configurationName):
+    with open('./config.json') as f:
+        config = json.load(f)
+    return config
+
+
+def replaceWorkspaceHome(config, properties):
+    for resource in config.get("resources"):
+        workspaceHome = properties["DATABRICKS"]["WORKSPACE_HOME"]
+        path = resource.get("properties").get("path")
+        newWorkspacePath = path.replace("%WORKSPACE_HOME%", workspaceHome)
+        resource["properties"]["path"] = newWorkspacePath
+        return config
+
+
+def writeNewConfig(config):
+    with open('config-new.json', 'w') as new_config:
+        json.dump(config, new_config, indent=4)
+
+
+config = getConfig("./config.json")
+properties = getProperties("stack.properties")
+replaceWorkspaceHome(config, properties)
+writeNewConfig(config)
